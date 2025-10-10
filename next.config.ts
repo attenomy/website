8dd6836f7508113186915cf7b3b4acbe0049141b
Saad <reach.saad@outlook.com>
2025-10-10 20:05:24 +0530

    DYNAMIC

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  output: 'export', // enables static HTML export
+  //output: 'export', // enables static HTML export
   eslint: {
     ignoreDuringBuilds: true,
   },
